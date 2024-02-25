d1232b4b84d83361555689fcc22ff7911f1be57f
vinniejipsk <141219700+vinniejipsk@users.noreply.github.com>
2024-02-25 22:57:13 +0800

    Temporary disabled userId

--- daos/posts.js
+++ daos/posts.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-  // userId: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "Users",
-  //   required: true,
-  // },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: false,
+  },
   title: {
     type: String,
     required: true,
